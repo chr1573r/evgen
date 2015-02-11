537bf1eb0e410555fd43fc080aa1837af1b2559c
chr1573r <christer@csdnserver.com>
2015-02-11 22:41:59 +0100

    V1

---
+++ evgen.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# EVGEN - Christer Jonassen
+
+from flask import Flask, render_template, request, url_for
+from functools import wraps
+from flask import request, Response
+import time
+import subprocess
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'changeme' and password == 'changeme'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'Beklager, ingen adgang :/', 401,
+    {'WWW-Authenticate': 'Basic realm="Givz credentialz plz"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Define a route for the default URL, which loads the form
+@app.route('/')
+@requires_auth
+def form():
+    return render_template('form_submit.html')
+
+# Define a route for the action of the form, for example '/hello/'
+# We are also defining which type of requests this route is 
+# accepting: POST requests in this case
+@app.route('/evgen/', methods=['POST'])
+@requires_auth
+def evgen():
+    timestamp = int(time.time())
+    sted=request.form['sted']
+    tidspunkt=request.form['tidspunkt']
+    ekstratekst=request.form['ekstratekst']
+    subprocess.check_call(['./evgen.sh', sted, tidspunkt, ekstratekst])
+    return render_template('form_action.html', sted=sted, tidspunkt=tidspunkt, ekstratekst=ekstratekst, timestamp=timestamp)
+
+# Run the app :)
+#app.debug = True
+
+if __name__ == '__main__':
+  app.run( 
+        host="0.0.0.0",
+        port=int("1337")
+  )
+
---
+++ evgen.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# run like this:
+#	./evgen.sh "Østsia" "17. november, Kl. 18.00-22.00" "Åpent for alle!"
+echo Lager overlay med teksten:
+
+echo
+echo $1
+echo $2
+echo $3
+echo $(pwd)
+
+#convert -background transparent -fill grey100 -font Typomoderno -size 851x315  -pointsize 24 -gravity southeast label:"$1 \n $2 \n $3 \n" output.png ; composite -quality 100 output.png kodekveld_blank.png output.png
+convert -background transparent -fill grey100 -font Typomoderno -size 784x295  -pointsize 24 -gravity southeast label:"$1 \n $2 \n $3 \n" ./static/output.png ; composite -quality 100 ./static/output.png ./static/kodekveld_nusize_blank.png ./static/output.png
+echo
+echo Ferdig!
